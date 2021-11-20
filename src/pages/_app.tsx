01532f513864ed2bb4240636513772ed2ae2c292
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2021-11-19 23:45:31 -0300

    create header and button components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.scss'
+import { Header } from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>ig.news</title>
       </Head>
+      <Header />
       <Component {...pageProps} />
     </>
   )
