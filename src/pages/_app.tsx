7ed7148f1d8ffb4fd9c86859ae3345d3e3cf9a33
brunoBrian <bruninho_brian@hotmail.com>
2021-11-21 00:47:14 -0200

    finalizando home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.scss'
@@ -7,9 +6,6 @@ import { Header } from '../components/Header'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>ig.news</title>
-      </Head>
       <Header />
       <Component {...pageProps} />
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,32 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.scss'
+import Head from 'next/head'
+import Image from 'next/image'
+
+import { SubscribeButton } from '../components/SubscribeButton'
+
+import styles from './Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <h1 className={styles.title}>Hello World</h1>      
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, Welcome </span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <Image src="/images/avatar.svg" alt="Girl coding" width={500} height={500} />
+
+      </main>
+    </>
   )
 }
 
