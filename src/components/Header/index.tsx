c1f0067a1aac8906e654767326849cdbb6142edf
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2021-12-20 16:57:16 -0300

    adjust nav links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.scss';
@@ -9,8 +10,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <Image src="/images/logo.svg" alt="ig.news" width={110} height={30} />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href='/' activeClass={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href='/posts' activeClass={styles.active} prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
