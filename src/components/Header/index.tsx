676eaa5f88fa0f94ddbf13353b08686a97d1e377
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2021-12-22 10:47:05 -0300

    some fixes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,14 +9,17 @@ export function Header() {
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src="/images/logo.svg" alt="ig.news" width={110} height={30} />
+
         <nav>
           <ActiveLink href='/' activeClass={styles.active}>
             <a>Home</a>
           </ActiveLink>
+
           <ActiveLink href='/posts' activeClass={styles.active} prefetch>
             <a>Posts</a>
           </ActiveLink>
         </nav>
+        
         <SignInButton />
       </div>
     </header>
